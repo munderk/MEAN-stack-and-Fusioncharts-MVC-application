5f4860741de8178e0f2ee2469252a1957b39f7fb
Rishabh Munde <munderishabh@gmail.com>
2016-03-14 16:39:22 +0530

    Added files via upload

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,1 +1,39 @@
+﻿
 
+
+//Author: Rishabh Munde
+
+var myApp = angular.module('myApp', ["ng-fusioncharts"]);
+
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) 
+{
+    console.log("Hello World from controller");
+
+      $http.get('/ptdata1').success(function(response) 
+      {
+        console.log("I got the data I requested");    //for debugging purposes. it will appear on on browser->more_tools->devTools->console
+        console.log(response);                        //same as above
+        var ChartParameters = 
+                {
+                    caption: "Def_Created",
+                    theme: "ocean",
+                    xAxisName: "Duration",
+                    xAxisNameFont: "Arial",
+                    xAxisNameFontBold: "1", 
+                    yAxisName: "Count",
+                    yAxisNameFont: "Arial",
+                    xAxisNameFontBold: "1",
+                    xAxisNameFontSize: "12",
+                    yAxisNameFontSize: "12",
+                    rotateValues: "0"
+                };
+        $scope.myDataSource =
+        {
+            chart: ChartParameters,
+            data: response
+        };
+
+       
+      });
+      $scope.myDataSource={};
+}]);﻿
